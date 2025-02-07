6d0e27ea9493a8ed260b864dbd7c00523e4f19ba
trungsliver <buiductrung.cva@gmail.com>
2025-02-07 19:29:32 +0700

    Lesson 1

---
+++ Lesson1/lesson1.py
@@ -0,0 +1,17 @@
+# Data Types - Kiểu dữ liệu
+    # Strings - Chuỗi ký tự / Xâu ký tự
+name = 'loopy'
+    # Int (integer) - số nguyên
+age = 2
+    # Float - số thực (số thập phân)
+score = 9.5
+    # Boolean - True/False, Đúng/Sai
+die = False
+
+# Các kiểu print: 4 kiểu
+    # cách 1: Dùng dấu +
+print('Họ tên: ' + name + ' Tuổi: ' + str(age))
+    # cách 2: Dùng dấu ,
+print('Điểm:', score, 'Qua đời:', die)
+    # cách 3: Dùng f - truyền dữ liệu vào string
+print(f'{name} được {score} điểm trong bài kiểm tra')
