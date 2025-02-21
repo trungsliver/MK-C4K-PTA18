7844656fb1f00bed546c3db8ba82847551f8bcf8
trungsliver <buiductrung.cva@gmail.com>
2025-02-21 20:52:01 +0700

    Lesson 3

--- Lesson3/main.py
+++ Lesson3/main.py
@@ -1,14 +1,15 @@
 from oop import Human, Student
+from oop import Animal, Dog
 
 stu1 = Student('Duc Minh', 12, 'male', 'Vinschool')
 stu2 = Student('Quang Huy', 14, 'male', 'Vinschool')
 
-print(stu1)
-# sử dụng phương thức của lớp cha
-stu1.introduce()
-stu1.display_info()
-# stu1.edit_age()
-stu1.edit_name('Loopy')
+# print(stu1)
+# # sử dụng phương thức của lớp cha
+# stu1.introduce()
+# stu1.display_info()
+# # stu1.edit_age()
+# stu1.edit_name('Loopy')
 
 # Đề bài:
 # Tạo class Animal gồm các thuộc tính: tên, loài
@@ -21,3 +22,11 @@ stu1.edit_name('Loopy')
 # Yêu cầu:
 # - Tạo class ở file oop.py
 # - Viết chương trình test tại file main.py
+
+a1 = Animal('Bơ', 'Chó')
+a1.display_info()
+a1.eat()
+
+dog1 = Dog('Mít', 'Chó', 'Shiba')
+dog1.display_info()
+dog1.edit_name('Sầu riêng')
--- Lesson3/oop.py
+++ Lesson3/oop.py
@@ -56,3 +56,42 @@ Trường học:     {self.school}
         self.name = new_name
         # Hiển thị lại thông tin sau chỉnh sửa
         self.display_info()
+
+# Luyện tập
+class Animal():
+    # Khởi tạo đối tượng với 2 thuộc tính
+    def __init__(self, ten, loai):
+        self.ten = ten
+        self.loai = loai
+    
+    # Phương thức giới thiệu
+    def display_info(self):
+        print(f'''
+========== ANIMAL =========
+Tên:    {self.ten}
+Loại:   {self.loai}
+===========================''')
+
+    # Phương thức eat 
+    def eat(self):
+        print(f'{self.ten} đang ăn')
+
+class Dog(Animal):
+    # Khởi tạo và thêm thuộc tính giong
+    def __init__(self, ten, loai, giong):
+        super().__init__(ten, loai)
+        self.giong = giong
+    
+    # Phương thức giới thiệu
+    def display_info(self):
+        print(f'''
+========== DOG =========
+Tên:    {self.ten}
+Loại:   {self.loai}
+Giong:  {self.giong}
+========================''')
+        
+    # Phương thức mới: chỉnh sửa tên
+    def edit_name(self, new_name):
+        self.ten = new_name
+        self.display_info()
