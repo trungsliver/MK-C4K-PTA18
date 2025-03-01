b428524e76b08cda8f7760277e49faa27705b873
trungsliver <buiductrung.cva@gmail.com>
2025-03-01 16:26:08 +0700

    Lesson 4

--- Lesson4/main.py
+++ Lesson4/main.py
@@ -1,16 +1,16 @@
-from PyQt6 import QtWidgets as qtw
+import sys
+from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6 import QtCore
 from PyQt6 import uic
-import sys 
 
-page = 1
-class Main(qtw.QMainWindow):
+class Main(QMainWindow):
     def __init__(self):
-        super(Main, self).__init__()
-        uic.loadUi("lesson4.ui", self)
-    
-                            
-if __name__ == '__main__':
-    app = qtw.QApplication(sys.argv)
+        super().__init__()
+        uic.loadUi('lesson4.ui', self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
     window = Main()
     window.show()
-    app.exec()
+
+    sys.exit(app.exec())
